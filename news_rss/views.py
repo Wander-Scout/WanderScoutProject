00f7140edf49ea83fed74b8baaeda0597269f6ed
Heinrich <heinrich.edric@cs.ui.ac.id>
2024-10-10 10:45:39 +0700

    Modified news_rss

--- news_rss/views.py
+++ news_rss/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import feedparser
 
-# Create your views here.
+
+########################
+'''News Feedparser'''
+########################
+def get_news():
+    feed_url = "https://jogja.antaranews.com/rss/pariwisata-budaya.xml"
+    feed = feedparser.parse(feed_url)
+    return feed.entries
+
+def news_view(request):
+    articles = get_news()
+    return render(request, 'news_.html', {'articles': articles})
+    ########################
