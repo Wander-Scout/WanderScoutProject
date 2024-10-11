283b29869997878209e70869c0980afc3bf238dc
davinawuy <alano.davin@office.ui.ac.id>
2024-10-11 23:16:36 +0700

    feat: creating model for tourist attraction

--- tourist_attraction/models.py
+++ tourist_attraction/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+import uuid
 
 class TouristAttraction(models.Model):
-        name = 
-        rating = 
-        type =
-        htmweekday =
-        htmweekend =
-        description =
-        gmapsurl =
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    type = models.CharField(max_length=100)
+    htmweekday = models.IntegerField()
+    htmweekend = models.IntegerField()
+    description = models.TextField()
+    gmapsurl = models.URLField(max_length=500)
+
+    def __str__(self):
+        return self.name
