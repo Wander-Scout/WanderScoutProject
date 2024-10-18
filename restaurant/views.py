e7ceb0af86871caaae4140e114ee91beb28ea567
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2024-10-18 09:39:07 +0700

    edit:fixing views.py in restaurant directory

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.urls import path
 from django.http import JsonResponse
 from .models import Restaurant
+from django.views.decorators.http import require_http_methods
+
 
 # Create your views here.
+@require_http_methods(["GET"])
 def get_all_restaurants(request):
-    restaurants = Restaurant.objects.all().values('name', 'food_preference', 'average_price', 'rating', 'atmosphere_type', 'food_variations')
+    restaurants = Restaurant.objects.all().values(
+        'name', 'food_preference', 'average_price', 'rating', 
+        'atmosphere_type', 'food_variations'
+    )
     return JsonResponse(list(restaurants), safe=False)
