405e56460c27fd95e8b0ff63885d3d3b4a33ed3c
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2024-10-18 19:45:44 +0700

    data:added unfinished login feature

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from main.views import login_user
+
 
 urlpatterns = [
+    path('login/', views.login_user, name='login'),
     path('', views.landing_page, name='landing_page'),
     path('home/', views.home_page, name='home_page'),
-    
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='login/')
 def landing_page(request):
     return render(request, 'landing_page.html') 
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Login successful.")
+            return redirect('home_page')  # redirect to the home page after login
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+
+    
