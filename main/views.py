8cd87ac5efc9d9aae41489f6b114820415a705a5
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-10-23 11:17:45 +0700

    feat: fix the customer reviews

--- main/views.py
+++ main/views.py
@@ -12,18 +12,23 @@ def landing_page(request):
 def home_page(request):
     return render(request, 'home_page.html')
 
-@require_http_methods(["POST"])
-def customer_reviews(request):
-    if request.method == 'POST':
-        form = CustomerReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.user
-            review.save()
-            messages.success(request, "Your review has been submitted!")
-            return redirect('customer_reviews')
-    else:
-        form = CustomerReviewForm()
-
+@require_http_methods(["GET"])
+def display_customer_reviews(request):
+    # Display reviews and form for GET requests
+    form = CustomerReviewForm()
     reviews = CustomerReview.objects.all().order_by('-created_at')
     return render(request, 'customer_reviews.html', {'form': form, 'reviews': reviews})
+
+@require_http_methods(["POST"])
+def submit_customer_review(request):
+    form = CustomerReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.user = request.user
+        review.save()
+        messages.success(request, "Your review has been submitted!")
+    else:
+        messages.error(request, "There was an error in your submission.")
+    
+    # Redirect to 'customer_reviews' after submitting
+    return redirect('customer_reviews')
