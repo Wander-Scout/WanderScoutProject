c1c346faa41647ffd7195f6008e25dca9f1e065b
davinawuy <alano.davin@office.ui.ac.id>
2024-10-25 18:22:12 +0700

    bugfix: encrypting password

--- authentication/signals.py
+++ authentication/signals.py
@@ -3,13 +3,26 @@ from django.contrib.auth.models import User, Group
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+def caesar_decrypt(text, shift):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha(): 
+            offset = ord('a') if char.islower() else ord('A')
+            decrypted_text += chr((ord(char) - offset - shift) % 26 + offset)
+        else:
+            decrypted_text += char 
+    return decrypted_text
+encryptpass = "wkrohwkxqghu"
+
+
+
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
     # Check if superuser already exists
     if not User.objects.filter(is_superuser=True).exists():
         # Define the superuser credentials
         superuser_username = 'thole'
-        superuser_password = 'tholethunder'
+        superuser_password = str(caesar_decrypt(encryptpass, 3))
 
         superuser = User.objects.create_superuser(
             username=superuser_username,
@@ -20,5 +33,3 @@ def create_superuser(sender, **kwargs):
 
         superuser.groups.add(admin_group)
 
-        print(f"Superuser created with username: {superuser_username} and password: {superuser_password}")
-        print("Superuser added to 'admin' group.")
