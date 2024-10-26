8e82a1e8fc4622c16d26cb825b26ee6fb8f4f6ec
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-10-27 01:42:41 +0700

    feat: adding deployment and fix a few functions

--- image_display/models.py
+++ image_display/models.py
@@ -8,3 +8,10 @@ class RSSItem(models.Model):
 
     def __str__(self):
         return self.title
+
+class ImageModel(models.Model):
+    image_url = models.URLField(max_length=500)
+    caption = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.caption
--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import CustomerReview
+from .models import AdminReply
 
 class CustomerReviewForm(forms.ModelForm):
     class Meta:
@@ -8,4 +9,12 @@ class CustomerReviewForm(forms.ModelForm):
         widgets = {
             'review_text': forms.Textarea(attrs={'placeholder': 'Write your review here...', 'rows': 3}),
             'rating': forms.Select(choices=[(i, f"{i} Stars") for i in range(1, 6)]),
+        }
+
+class AdminReplyForm(forms.ModelForm):
+    class Meta:
+        model = AdminReply
+        fields = ['reply_text']
+        widgets = {
+            'reply_text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your reply here...'}),
         }
