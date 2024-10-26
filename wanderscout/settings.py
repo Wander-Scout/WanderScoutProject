aa677ac93cfdf8308575e2039fa76dadce50a9b9
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-10-27 02:10:00 +0700

    feat: fix the settings.py

--- wanderscout/settings.py
+++ wanderscout/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://samuella-putri-wanderscout.pbp.cs.ui.ac.id/", "https://samuella-putri-wanderscout.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/alano.davin/wanderscout", "https://pbp.cs.ui.ac.id/alano.davin/wanderscout"]
