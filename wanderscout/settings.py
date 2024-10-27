42651e6237fc385686104914528cb710a22d8ca5
davinawuy <alano.davin@office.ui.ac.id>
2024-10-27 22:00:33 +0700

    bugfix: fixing debug settings and templates

--- wanderscout/settings.py
+++ wanderscout/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lmo57ez)%!z+#6v75qwsgex2r@l077n!@gc(87^f2n^2k=%ctu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alano-davin-wanderscout.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/alano.davin/wanderscout", "https://pbp.cs.ui.ac.id/alano.davin/wanderscout"]
