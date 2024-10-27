4d6a4c485d14093d81a02e715a1d5c0c993bcfe0
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2024-10-27 21:21:35 +0700

    style: added comments

--- restaurant/templatetags/custom_tags.py
+++ restaurant/templatetags/custom_tags.py
@@ -1,7 +1,7 @@
 from django import template
 
-register = template.Library()
+register = template.Library() # set up a template library instance so custom filters can be registered
 
 @register.filter
 def is_admin(user):
-    return user.groups.filter(name="admin").exists()
+    return user.groups.filter(name="admin").exists() # check if the user is part of the 'admin' group
