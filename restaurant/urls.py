45b23c5e69fc0919f85ab68b67fa8ea03f43aba1
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2024-10-27 18:15:29 +0700

    feat: added edit feature

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.display_restaurants_as_cards, name='restaurants'),  # Display restaurant list
     path('api/', views.get_restaurants, name='get_restaurants'),  # API for restaurants
+    path('restaurant/<uuid:restaurant_id>/data/', views.get_restaurant_data, name='get_restaurant_data'),
+    path('restaurant/<uuid:restaurant_id>/update/', views.update_restaurant, name='update_restaurant'),
     path('restaurant/<uuid:restaurant_id>/', views.restaurant_detail, name='restaurant_detail'),  # Restaurant detail
     path('api/add_restaurant/', views.add_restaurant, name='add_restaurant'),
     path('api/delete_restaurant/<uuid:restaurant_id>/', views.delete_restaurant, name='delete_restaurant'),
+
     
 ]
