0b95d765bd49cc9c0d38114f1414d59c0cf8cb63
davinawuy <alano.davin@office.ui.ac.id>
2024-10-27 20:06:29 +0700

    style: commenting the image display for better readability

--- image_display/models.py
+++ image_display/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class RSSItem(models.Model):
+    # Info from the rss feed
     title = models.CharField(max_length=200)
     description = models.TextField()
     pub_date = models.DateTimeField()
     image_url = models.URLField(max_length=500, blank=True, null=True)
+    #pulled based of the XML in the rss feed
 
     def __str__(self):
         return self.title
