3603ede0f4e037e6f355866f49dd513d5a1e04cf
davinawuy <alano.davin@office.ui.ac.id>
2024-12-13 15:58:03 +0700

    fix: register #2

--- wanderscout/settings.py
+++ wanderscout/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alano-davin-wanderscout.pbp.cs.ui.ac.id","10.0.2.2"]
 CSRF_TRUSTED_ORIGINS = [
     'https://alano-davin-wanderscout.pbp.cs.ui.ac.id',
+    'http://localhost:63191',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
 ]
@@ -151,6 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://alano-davin-wanderscout.pbp.cs.ui.ac.id',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://localhost:63191',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
